e25aa89f04388174463d51fd978d7582d1742b54
jessicakoo9 <jessicakoo123@gmail.com>
2026-02-09 09:56:17 -0500

    Implement Rectangle class for Shape interface

---
+++ rectangle.java
@@ -0,0 +1,37 @@
+public class Rectangle implements Shape {
+
+    private double length;
+    private double width;
+
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+
+    public double getLength() {
+        return length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+
+    @Override
+    public double getArea() {
+        return length * width;
+    }
+
+
+    @Override
+    public double getPerimeter() {
+        return 2 * (length + width);
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle with length = " + length + ", width = " + width;
+    }
+}
